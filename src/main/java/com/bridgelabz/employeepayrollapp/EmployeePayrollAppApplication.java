226b26fe7579705c0a3b61427a83bbd3233a01e8
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-03 20:52:35 +0530

    ADDED: Use slf4j from lombok library for logging

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,12 +2,15 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee payroll app started");
 	}
 
 }
