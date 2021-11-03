e4c3302ce259ef1f3ff907ca9da0c129e20642eb
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-03 22:58:30 +0530

    REFACTORED: Name pattern

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.Pattern;
 
 public class EmployeePayrollDTO {
 
-	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$",message = "employee name Invalid")
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
 	public String name;
 	
 	@Min(value = 500,message = "Min wage should be more than 500")
